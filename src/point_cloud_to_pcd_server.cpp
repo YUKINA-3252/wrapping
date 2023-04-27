0eb932ea3ed0c96c4540a31d3b8448cc6811b352
YUKINA-3252 <yukki1013.rock@gmail.com>
2023-04-27 18:11:22 +0900

    Fix typo

--- src/point_cloud_to_pcd_server.cpp
+++ src/point_cloud_to_pcd_server.cpp
@@ -19,79 +19,80 @@ public:
 };
 
 Server::Server() {
+  PCL Nodelet::OnInit();
   srv_ = boost::make_shared <dynamic_reconfigure::Server<Config> > (*pnh_);
   dynamic_reconfigure::Server<Config>::CallbackType f = boost::bind(&PointCloudToPCD::configCallback, this, _1, _2);
   srv_->setCallback (f);
   tf_listener_ = jsk_recognition_utils::TfListenerSingleton::getInstance();
-  if(binary_)
-    {
-      if(compressed_)
-        {
-          ROS_INFO_STREAM ("Saving as binary compressed PCD");
-        }
-      else
-        {
-          ROS_INFO_STREAM ("Saving as binary PCD");
-        }
-    }
-  else
-    {
-      ROS_INFO_STREAM ("Saving as binary PCD");
-    }
+//   if(binary_)
+//     {
+//       if(compressed_)
+//         {
+//           ROS_INFO_STREAM ("Saving as binary compressed PCD");
+//         }
+//       else
+//         {
+//           ROS_INFO_STREAM ("Saving as binary PCD");
+//         }
+//     }
+//   else
+//     {
+//       ROS_INFO_STREAM ("Saving as binary PCD");
+//     }
 }
+
 // void Server::convertServer (wrapping::Convert::Request &req,
 //                             wrapping::Convert::Response &res)
-void Server::convertServer ()
-  {
-    pcl::PCLPointCloud2::ConstPtr cloud;
-    cloud = ros::topic::waitForMessage<pcl::PCLPointCloud2>("input", *pnh_);
-    if ((cloud->width * cloud->height) == 0)
+void Server::convertServer () {
+  pcl::PCLPointCloud2::ConstPtr cloud;
+  cloud = ros::topic::waitForMessage<pcl::PCLPointCloud2>("input", *pnh_);
+  if ((cloud->width * cloud->height) == 0)
     {
       return;
     }
 
-    ROS_INFO ("Received %d data points in frame %s with the following fields: %s",
+  ROS_INFO ("Received %d data points in frame %s with the following fields: %s",
             (int)cloud->width * cloud->height,
             cloud->header.frame_id.c_str (),
             pcl::getFieldsList (*cloud).c_str ());
 
-    Eigen::Vector4f v = Eigen::Vector4f::Zero ();
-    Eigen::Quaternionf q = Eigen::Quaternionf::Identity ();
-    if (!fixed_frame_.empty ()) {
-        if (!tf_listener_->waitForTransform (fixed_frame_, cloud->header.frame_id, pcl_conversions::fromPCL (cloud->header).stamp, ros::Duration (duration_))) {
-        ROS_WARN("Could not get transform!");
-        return;
-      }
-      tf::StampedTransform transform_stamped;
-      tf_listener_->lookupTransform (fixed_frame_, cloud->header.frame_id,  pcl_conversions::fromPCL (cloud->header).stamp, transform_stamped);
-      Eigen::Affine3d transform;
-      tf::transformTFToEigen(transform_stamped, transform);
-      v = Eigen::Vector4f::Zero ();
-      v.head<3> () = transform.translation ().cast<float> ();
-      q = transform.rotation ().cast<float> ();
+  Eigen::Vector4f v = Eigen::Vector4f::Zero ();
+  Eigen::Quaternionf q = Eigen::Quaternionf::Identity ();
+  if (!fixed_frame_.empty ()) {
+    if (!tf_listener_->waitForTransform (fixed_frame_, cloud->header.frame_id, pcl_conversions::fromPCL (cloud->header).stamp, ros::Duration (duration_))) {
+      ROS_WARN("Could not get transform!");
+      return;
     }
-
-    std::stringstream ss;
-    ss << prefix_ << cloud->header.stamp << ".pcd";
-    ROS_INFO ("Data saved to %s", ss.str ().c_str ());
-
-    pcl::PCDWriter writer;
-    if(binary_)
-    {
-      if(compressed_)
-      {
-        writer.writeBinaryCompressed (ss.str (), *cloud, v, q);
-      }
-      else
-      {
-        writer.writeBinary (ss.str (), *cloud, v, q);
-      }
-    }
-    else
-    {
-      writer.writeASCII (ss.str (), *cloud, v, q, 8);
-    }
+    tf::StampedTransform transform_stamped;
+    tf_listener_->lookupTransform (fixed_frame_, cloud->header.frame_id,  pcl_conversions::fromPCL (cloud->header).stamp, transform_stamped);
+    Eigen::Affine3d transform;
+    tf::transformTFToEigen(transform_stamped, transform);
+    v = Eigen::Vector4f::Zero ();
+    v.head<3> () = transform.translation ().cast<float> ();
+    q = transform.rotation ().cast<float> ();
   }
+
+  std::stringstream ss;
+  ss << prefix_ << cloud->header.stamp << ".pcd";
+  ROS_INFO ("Data saved to %s", ss.str ().c_str ());
+
+  pcl::PCDWriter writer;
+  // if(binary_)
+  // {
+  //   if(compressed_)
+  //   {
+  //     writer.writeBinaryCompressed (ss.str (), *cloud, v, q);
+  //   }
+  //   else
+  //   {
+  //     writer.writeBinary (ss.str (), *cloud, v, q);
+  //   }
+  // }
+  // else
+  // {
+  //   writer.writeASCII (ss.str (), *cloud, v, q, 8);
+  // }
+}
 
 void Server::loop() {
   ros::Rate loop_rate(30);
